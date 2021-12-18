e46b5daadadd5ef9236d925c5829e75f7133f050
leo <lavalle.18384929@studenti.uniroma1.it>
2021-12-18 16:38:58 +0100

    j

--- small_data/architectures/densenet.py
+++ small_data/architectures/densenet.py
@@ -147,7 +147,7 @@ class DenseNet(nn.Module):
                     ("conv0", nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
                     ("norm0", nn.BatchNorm2d(num_init_features)),
                     ("relu0", nn.ReLU(inplace=True)),
-                    ("pool0", nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+                    ("pool0", nn.MaxPool2d(kernel_size=3, stride=2, padding=1,ceil_mode=True)),
                 ]
             )
         )
