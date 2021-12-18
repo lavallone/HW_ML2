34b868639c5e75cb8c963900a35911773e183cb7
leo <lavalle.18384929@studenti.uniroma1.it>
2021-12-18 13:06:25 +0100

    gg

--- small_data/architectures/se_res_next.py
+++ small_data/architectures/se_res_next.py
@@ -125,7 +125,7 @@ class SEResNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
 
-       #x = self.avgpool(x)
+        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
