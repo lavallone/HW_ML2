4983ae41225f64bb4cdf62ec25dfcf3c030c2c6a
leo <lavalle.18384929@studenti.uniroma1.it>
2021-12-18 16:47:26 +0100

    f

--- small_data/architectures/densenet.py
+++ small_data/architectures/densenet.py
@@ -111,7 +111,7 @@ class _Transition(nn.Sequential):
         self.add_module("norm", nn.BatchNorm2d(num_input_features))
         self.add_module("relu", nn.ReLU(inplace=True))
         self.add_module("conv", nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False))
-        self.add_module("pool", nn.AvgPool2d(kernel_size=2, stride=2, ceil_mode=True))
+        self.add_module("pool", nn.AvgPool2d(kernel_size=2, stride=2, ceil_mode=True)) # sempre problematico! --> ho dovuto impostare ceil_mode=True
 
 
 class DenseNet(nn.Module):
